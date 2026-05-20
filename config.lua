1eea17a8a117fafeb596237d0246c05e2e77cbff
Ws Scripts <58374931+itzwhis@users.noreply.github.com>
2026-05-20 22:59:40 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+-- Command to open the spawner menu
+Config.Command = 'object'
+
+-- Admin permission required (rsg-core ace permission group)
+Config.AdminGroup = 'admin'
+
+-- Max distance (meters) for laser raycast placement
+Config.LaserDistance = 50.0
+
+-- Render distance for spawned saved objects
+Config.RenderDistance = 150.0
+
+-- Step size for keyboard fine adjustment in placement mode
+Config.MoveStep = 0.05
+Config.RotateStep = 2.5
